5f38c76dcd3dae0d8c1e5f2124fe612ff6f4e8b5
Aran Dunkley <aran@organicdesign.co.nz>
2013-10-19 13:07:00 +0000

    del everything but bg
    git-svn-id: svn+ssh://organicdesign.co.nz/bitgroup@1010 5e2f9ad9-d71a-4c5e-ba50-a8a02cc83e87

--- dovecot-1.x-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-openssl genrsa -out /etc/ssl/private/dovecot.pem 1024
-openssl req -new -key /etc/ssl/private/dovecot.pem -out /tmp/csr
-openssl x509 -in /tmp/csr -out /etc/ssl/certs/dovecot.pem -req -signkey /etc/ssl/private/dovecot.pem -days 999
-
--- dovecot-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-openssl genrsa -out /etc/dovecot/private/dovecot.pem 1024
-openssl req -new -key /etc/dovecot/private/dovecot.pem -out /tmp/csr
-openssl x509 -in /tmp/csr -out /etc/dovecot/dovecot.pem -req -signkey /etc/dovecot/private/dovecot.pem -days 999
-
